71740155c40fe913d7abb30dbb8c7939997e02b4
heyhenry <henryyy3@hotmail.com>
2023-12-12 10:54:54 +0800

    Altering both sep and end parameters.

--- CS50-Intro-Python/hello.py
+++ CS50-Intro-Python/hello.py
@@ -2,8 +2,11 @@
 name = input("What's your name? ")
 
 # Say hello to user
-# The usage of the innate parameter 'end=' to cancel out its default newline execution and 
+# The usage of the innate parameter 'end=' (also known as named parameters) to cancel out its default newline execution and 
 # instead provide no space so that the next print statement is displayed on the same line, 
 # portraying a sense of continuity or concatenation.
-print("Hello, ", end="")
+print("hello ", end="")
+print(name)
+print("---")
+print("hello,", name, sep="!!!", end="")
 print(name)
